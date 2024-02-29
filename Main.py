0ea8c2ed713039b0863de09dec19380294a3f578
Lionixes <161773997+Lionixes@users.noreply.github.com>
2024-03-01 00:10:48 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 users=[]
-student_name = input("please enter your name: ")
-student_id = input("please enter your ID :")
+num =int(input("please enter how many students"))
+for i in range(num):
+    student_name = input("please enter your name: ")
+    student_id = input("please enter your ID :")
+    student_age = int(input("please enter student age: ")(
+    users.append((student_id, studen_name, student_age))
 
-users.append((student_id, studen_name))
-#add your code here
 
-
-print(f"student ID: {users[i][0]}\nstudent name: {users[i][1]}")
+for i in users:
+    print(f"student ID: {users[i][0]}\nstudent name: {users[i][1]}\n age: {users[i][2]}")
