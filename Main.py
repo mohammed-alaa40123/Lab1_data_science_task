da0358c677b8f6f595a22070e5b00d5006d70795
mohammed-alaa40123 <66653422+mohammed-alaa40123@users.noreply.github.com>
2024-02-29 23:49:32 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+users=[]
+student_name = input("please enter your name: ")
+student_id = input("please enter your ID :")
+
+users.append((student_id, studen_name))
+#add your code here
+
+
+print(f"student ID: {users[i][0]}\nstudent name: {users[i][1]}")
